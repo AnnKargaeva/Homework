ace787e64451dc7c7a253bb58f9bf66f620a0409
Anna <annet0924@yandex.ru>
2022-12-07 17:31:37 +0300

    Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,17 @@
+﻿int a;
+
+
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+if (a < 100)
+{
+    Console.Write ($"Третьей цифры нет");
+}
+else 
+{
+    int b = a;
+    while (b / 100 >= 10)
+    b /= 10;
+    Console.Write ($"{a} -> {b % 10}");
+}
