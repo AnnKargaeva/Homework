7f26cbc2e73b5d641cf064b383ab9ef1ac4af001
Anna <annet0924@yandex.ru>
2022-12-07 13:56:52 +0300

    Homework solutions

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -3,7 +3,7 @@
 Console.Write($"Введите число: ");
 int.TryParse(Console.ReadLine()!, out a);
 
-for (int index = 1; index <= a; index ++)
+for (int index = 2; index <= a; index += 2)
 {
     if (index % 2 ==0)
         Console.Write($"{index} ");
