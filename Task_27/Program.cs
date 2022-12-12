18ac0a2444f845486ccacee5b8b5b43552c5e851
Anna <annet0924@yandex.ru>
2022-12-12 19:33:13 +0300

    Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,14 @@
+﻿int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine(), out a);
+
+int b = a;
+int sum = 0;
+
+while (b > 0)
+{
+    sum += b % 10;
+    b /= 10;
+}
+
+Console.Write($"{a} -> {sum}");
