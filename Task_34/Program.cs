fec7c625e9a2bd664e32a6c4eb824467e99c5ffd
Anna <annet0924@yandex.ru>
2022-12-14 16:24:24 +0300

    Task_34

---
+++ Task_34/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = new int [4];
+for (int i = 0; i < array.Length; i++)
+{ 
+    array [i] = new Random().Next(99, 1000);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+    if ( array[i] % 2 == 0)
+        count++;
+Console.WriteLine($"Количество четных чисел в массиве: {count}");
