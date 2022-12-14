315a5b27381325c9c161ab708b9a0417dc5e4190
Anna <annet0924@yandex.ru>
2022-12-14 17:29:58 +0300

    Task_38

---
+++ Task_38/Program.cs
@@ -0,0 +1,18 @@
+﻿double [] array = new double [4];
+for (int i = 0; i < array.Length; i++)
+{ 
+    array [i] = new Random().Next(1000) / 10.0;
+    Console.Write($"{array[i]}   ");
+}
+Console.WriteLine();
+
+double min = array[0];
+double max = array[0];
+for (int i = 0; i < array.Length; i++)
+    if (array[i] < min)
+        min = array[i];
+    else if (array[i]>max)
+        max = array[i];
+
+
+Console.WriteLine($"Разница max и min ->  {max-min}");
