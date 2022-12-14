a2873063b332fd99c292b3a6e7c75b852389b0d5
Anna <annet0924@yandex.ru>
2022-12-14 17:09:53 +0300

    Task_36

---
+++ Task_36/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = new int [4];
+for (int i = 0; i < array.Length; i++)
+{ 
+    array [i] = new Random().Next(-100, 100);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+    if (i % 2 != 0)
+        sum = sum + array[i];
+Console.WriteLine($"Сумма нечетных индексов: {sum}");
