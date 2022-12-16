33b43d2e3d9dea1b3230005510356e3beb3512d8
Anna <annet0924@yandex.ru>
2022-12-16 14:29:49 +0300

    task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string[] numbers = Console.ReadLine()!.Split(' ');
+int[] array = new int [numbers.Length];
+
+for(int i = 0; i < numbers.Length; i++)
+
+    int.TryParse(numbers[i], out array[i]);
+    
+int count = 0;
+foreach (var i in array)
+    if (i > 0)
+        count++;
+
+Console.Write($"{count} чисел больше 0");
+
+
