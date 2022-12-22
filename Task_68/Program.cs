c06b9ac3111c94b2d004dcc22462039f95969bde
Anna <annet0924@yandex.ru>
2022-12-22 13:11:04 +0300

    task 68

---
+++ Task_68/Program.cs
@@ -0,0 +1,12 @@
+﻿int m = 3, n = 2;
+Console.Write($"{Accerman(m, n)}");
+
+int Accerman(int m, int n)
+{
+    if (m==0)
+        return n + 1;
+    else if (n==0)
+        return Accerman(m-1, 1);
+    else 
+        return Accerman(m-1, Accerman(m, n - 1));
+}
