f69c3804d960b7879606d89c959bbf3dd6ffa29a
Anna <annet0924@yandex.ru>
2022-12-22 12:47:23 +0300

    task 66

---
+++ Task_66/Program.cs
@@ -0,0 +1,9 @@
+﻿int m = 1, n = 10;
+Console.Write($"{Summ(n, 1)}");
+
+string Summ(int left, int right)
+{
+    if (left >= right)
+        return left;
+    return left + Summ(left + 1, right);
+}
